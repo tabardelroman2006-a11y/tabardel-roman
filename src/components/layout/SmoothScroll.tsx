6213f0dc1ab310a105c009ace19d8caedf556402
Roman Tabardel <tabardelroman2006@gmail.com>
2026-05-16 14:54:36 +0200

    fix: migrate @studio-freight/lenis to lenis

--- src/components/layout/SmoothScroll.tsx
+++ src/components/layout/SmoothScroll.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, type ReactNode } from 'react'
-import Lenis from '@studio-freight/lenis'
+import Lenis from 'lenis'
 
 export function SmoothScroll({ children }: { children: ReactNode }) {
   useEffect(() => {
